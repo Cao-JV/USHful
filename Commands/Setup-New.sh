3f3f81c9d5653ce61bca4b5a4e021c0331ddb55c
cao <cao@jaggedverge.com>
2023-03-21 22:06:25 -0400

    Added a commands directory Added a Setup command - to add Software on a clean install Modified Settings - Added more Added SSH key Generator w/ registration

--- Commands/Setup-New.sh
+++ Commands/Setup-New.sh
@@ -5,15 +5,34 @@
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-DIR_HOME="/home/me/"
-DIR_DEVELOPMENT="${DIR_HOME}dev/"
-DIR_DOCUMENTS="${DIR_HOME}/Documents"
-DIR_LOGS="${DIR_HOME}.local/log/"
-DIR_SCRIPTS="${DIR_HOME}scripts/"
-DIR_SSH_KEYS="${DIR_HOME}.ssh/"
-EMAIL_SYSTEM_FROM="admin@domain.co.nz"
-EMAIL_ALERT_LIST="user1@domain.au;user2@home.com"
+# Setup desired software on System
+# Assuming Fedora 37
 
-SHOULD_EMAIL_AV=0
+# Upgrade to newest Kernel, etc.
+sudo dnf upgrade
 
-USER_EMAIL="user@localhost"
+# Address the damn NVidia shit
+sudo dnf install akmod-nvidia
+
+# Add Required Repos
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+
+# Unfilter Flathub
+sudo flatpak remote-delete flathub
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Entertainment
+sudo dnf install lutris
+wget https://github.com/doitsujin/dxvk/releases/download/v1.3.1/dxvk-1.3.1.tar.gz 
+tar -xf dxvk-1.3.1.tar.gz -C /home/cao/.local/share/lutris/runtime/dxvk/
+mv /home/cao/.local/share/lutris/runtime/dxvk/dxvk-v1.3.1 /home/cao/.local/share/lutris/runtime/dxvk/v1.3.1
+
+
+# Virtual machinery 
+sudo dnf install virt-manager 
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Develry
+curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
+bash install_nvm.sh
--- Commands/generate-ssh-key.sh
+++ Commands/generate-ssh-key.sh
@@ -5,15 +5,21 @@
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-DIR_HOME="/home/me/"
-DIR_DEVELOPMENT="${DIR_HOME}dev/"
-DIR_DOCUMENTS="${DIR_HOME}/Documents"
-DIR_LOGS="${DIR_HOME}.local/log/"
-DIR_SCRIPTS="${DIR_HOME}scripts/"
-DIR_SSH_KEYS="${DIR_HOME}.ssh/"
-EMAIL_SYSTEM_FROM="admin@domain.co.nz"
-EMAIL_ALERT_LIST="user1@domain.au;user2@home.com"
+# Create a new ssh key
+source ../.settings.sh
 
-SHOULD_EMAIL_AV=0
+ENCRYPTION_TYPE="ed25519"
+KEY_EMAIL=${USER_EMAIL}
 
-USER_EMAIL="user@localhost"
+if [ ${1} == "" ]
+then
+    echo "Must specify a password file!"
+    exit;
+fi
+FILE_NAME="${DIR_SSH_KEYS}${1}"
+
+
+ssh-keygen -t ed25519 -C "${KEY_EMAIL}" -f ${FILE_NAME}
+
+eval "$(ssh-agent -s)"
+ssh-add ${FILE_NAME}
